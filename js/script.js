6986c3a673bda5a25b3fb70cda9cbb823241de7d
Dmitrii Ivanov <d-marick@mail.ru>
2021-04-10 19:47:30 +0300

    tasks 6-13

--- js/script.js
+++ js/script.js
@@ -1,90 +1,91 @@
 'use strict';
 
+let money;
+
 let isNumber = function (money) {
   return !isNaN(parseFloat(money)) && isFinite(money);
 };
-
-
-let money,
-  income = '500';
-let addExpenses = 'Интернет, Такси, Коммуналка';
-let deposit;
-let mission = 200000;
-let period = 6;
 
 let start = function () {
   do {
     money = prompt('Ваш месячный доход?');
   } while (!isNumber(money));
-  console.log(money);
-
 };
 
 start();
-let expenses = [];
-let amount = [];
-let spendings = 0;
-let accumulatedMonth;
 
 
 let appData = {
+  expensesList: [],
+  amount: [],
   budget: money,
+  deposit: false,
+  spendings: 0,
+  mission: 200000,
   budgetDay: 0,
   budgetMonth: 0,
-  expensesMonth: 0,
-  getExpensesMonth: function () {
-
+  expensesMonth: 0, //обязательные траты
+  addExpenses: prompt('Перечислите возможные расходы за рассчитываемый период через запятую'),
+  asking: function () {
     for (let i = 0; i < 2; i++) {
-      expenses[i] = prompt('Введите обязательную статью расходов?');
-      amount[i] = prompt('Во сколько это обойдется?');
-      while (isNaN(parseFloat(amount[i]))) {
-        amount[i] = prompt('Во сколько это обойдется?');
+      appData.expensesList[i] = prompt('Введите обязательную статью расходов?');
+      let a = appData.expensesList[i];
+      appData.amount[i] = prompt('Во сколько это обойдется?');
+      let b = appData.amount[i];
+      appData.expenses[a] = +b;
+      while (isNaN(parseFloat(appData.amount[i]))) {
+        appData.amount[i] = prompt('Во сколько это обойдется?');
       }
-      spendings += Number(amount[i]);
+      appData.spendings += Number(appData.amount[i]);
     }
-    console.log('Расходы за месяц: ', spendings);
+  },
+  expenses: {},
+  getExpensesMonth: function () {
+    let expensesSum = 0;
+    for (let prop in appData.expenses) {
+      expensesSum += appData.expenses[prop];
+    }
+    appData.expensesMonth = expensesSum;
   },
 
-  getAccumulatedMonth: function () {
-    return money - spendings;
+  getBudget: function () {
+    appData.budgetMonth = money - appData.expensesMonth;
+    appData.budgetDay = Math.floor(appData.budgetMonth / 30);
   },
 
   getTargetMonth: function () {
-    return Math.ceil(mission / accumulatedMonth);
+    return Math.ceil(appData.mission / appData.budgetMonth);
   },
 
-  getStatusIncome: function (budgetDay) {
-    if (budgetDay >= 1200) {
+  getStatusIncome: function () {
+    if (appData.budgetDay >= 1200) {
       console.log('У вас высокий уровень дохода');
-    } else if (budgetDay > 600 && budgetDay < 1200) {
+    } else if (appData.budgetDay > 600 && appData.budgetDay < 1200) {
       console.log('У вас средний уровень дохода');
-    } else if (budgetDay <= 600 && budgetDay > 0) {
+    } else if (appData.budgetDay <= 600 && appData.budgetDay > 0) {
       console.log('К сожалению у вас уровень дохода ниже среднего');
-    } else if (budgetDay < 0) {
+    } else if (appData.budgetDay < 0) {
       console.log('Что то пошло не так');
     }
   },
 
-}; // end of appData
+}; //end app
 
-accumulatedMonth = appData.getAccumulatedMonth();
-
-addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую');
-deposit = confirm('Есть ли у вас депозит в банке?');
-
+appData.asking();
 appData.getExpensesMonth();
+appData.getBudget();
 
-addExpenses = addExpenses.toLowerCase();
-let addExpensesArr = addExpenses.split(', ');
-console.log('Возможные расходы: ' + addExpensesArr);
 
+console.log('Расходы за месяц: ' + appData.budgetMonth);
 if (appData.getTargetMonth() > 0) {
   console.log('Cрок достижения цели: ', appData.getTargetMonth());
 } else {
   console.log('Цель не будет достигнута');
 }
+appData.getStatusIncome();
 
-let budgetDay = Math.floor(accumulatedMonth / 30);
-console.log('Бюджет на день: ', budgetDay);
+console.log('Наша программа включает в себя данные: ');
+for (let key in appData) {
+  console.log(key + ': ' + appData[key]);
 
-appData.getStatusIncome(budgetDay);
+}
